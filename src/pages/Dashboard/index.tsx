9eb9f3e7742358a006587099a5f9f788870ff0e3
Phillippe Rocha <phillipperocha@gmail.com>
2020-06-23 20:51:34 -0300

    Next appointment style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -9,6 +18,9 @@ import { useAuth } from '../../hooks/auth';
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
 
+  // O Content será exibido o conteúdo abaixo do Header
+  // O Schedule sao os agendamentos a esquerda
+  // E o calendar é o pequeno calendário a direita que será feito a posteriori
   return (
     <Container>
       <Header>
@@ -16,7 +28,10 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="BarberShop" />
 
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
+            <img
+              src="https://avatars2.githubusercontent.com/u/32747713?s=460&u=cf48f7d4525ec7fece84e3251c5837d45bc81115&v=4"
+              alt={user.name}
+            />
 
             <div>
               <span>Welcome,</span>
@@ -29,6 +44,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/32747713?s=460&u=cf48f7d4525ec7fece84e3251c5837d45bc81115&v=4"
+                alt={user.name}
+              />
+
+              <strong>{user.name}</strong>
+              <span>
+                <FiClock />
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -5,7 +5,6 @@ import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { Link, useHistory } from 'react-router-dom';
 
-import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
