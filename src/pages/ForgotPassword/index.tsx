eba77f51be2905e52e96f6a67b11db90395574cb
Phillippe Rocha <32747713+phillipperocha@users.noreply.github.com>
2020-07-24 10:58:29 -0300

    Fixing Back to Login button on Recovery Password

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -86,7 +86,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Back to login
           </Link>
